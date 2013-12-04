f3635144257ebf35ce354299f5bf36941d227f48
Patrick Boe <patrick@pescaterian>
2013-12-04 07:08:08 -0500

    created a tool to explore the way the annotate-stx function works

--- scry.rkt
+++ scry.rkt
@@ -1,7 +1,9 @@
-(require racket/format
-         gui-debugger/annotator)
+#lang racket
 
-(annotate-stx (expand-syntax-to-top-form #'1)
+(require racket/format gui-debugger/annotator)
+
+(define (explore-annotate-stx stx)
+  (annotate-stx (expand-syntax-to-top-form (expand-syntax stx))
               (lambda (debug annotated original is-tail?)
                 (printf "BREAK-WRAP call.~ndebug:~n~s~nannotated:~n~s~noriginal:~n~s~nis-tail?~n~s"
                         debug
@@ -17,4 +19,6 @@
                 (printf "RECORD-TOP-LEVEL-ID call.~nuse-case:~n~s~nbound-stx:~n~s~nbinding-stx:~n~s~n"
                         use-case
                         bound-stx
-                        binding-stx)))
+                        binding-stx))))
+
+(explore-annotate-stx #'5)
