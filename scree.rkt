1d7176b65f7e915be7f3fc43914010f24f83662e
Patrick Boe <pboe@home>
2013-12-05 21:09:59 -0500

    found some syntax that gets somewhat annotated and refined exploratory function

---
+++ scree.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(require (for-syntax "scry.rkt"))
+
+(define-syntax littered
+  (lambda (stx)
+    (litter-with-printfs (cadr (syntax->list stx)))))
+
+(littered a)
--- scry.rkt
+++ scry.rkt
@@ -1,24 +1,25 @@
-#lang racket
-
+#lang racket 
 (require racket/format gui-debugger/annotator)
 
-(define (explore-annotate-stx stx)
-  (annotate-stx (expand-syntax-to-top-form (expand-syntax stx))
-              (lambda (debug annotated original is-tail?)
-                (printf "BREAK-WRAP call.~ndebug:~n~s~nannotated:~n~s~noriginal:~n~s~nis-tail?~n~s"
-                        debug
-                        annotated
-                        original
-                        is-tail?))
-              (lambda (use-case bound-stx binding-stx)
-                (printf "RECORD-BOUND-ID call.~nuse-case:~n~s~nbound-stx:~n~s~nbinding-stx:~n~s~n"
-                        use-case
-                        bound-stx
-                        binding-stx))
-              (lambda (use-case bound-stx binding-stx)
-                (printf "RECORD-TOP-LEVEL-ID call.~nuse-case:~n~s~nbound-stx:~n~s~nbinding-stx:~n~s~n"
-                        use-case
-                        bound-stx
-                        binding-stx))))
+(provide litter-with-printfs)
 
-(explore-annotate-stx #'5)
+(define (litter-with-printfs stx) 
+      (let-values ([(annotated breakmap)
+                    (annotate-stx stx
+                        (lambda (debug annotated original is-tail?)
+                          (printf "BREAK-WRAP call.~ndebug:~n~s~nannotated:~n~s~noriginal:~n~s~nis-tail?~n~s"
+                                  debug
+                                  annotated
+                                  original
+                                  is-tail?))
+                        (lambda (use-case bound-stx binding-stx)
+                          (printf "RECORD-BOUND-ID call.~nuse-case:~n~s~nbound-stx:~n~s~nbinding-stx:~n~s~n"
+                                  use-case
+                                  bound-stx
+                                  binding-stx))
+                        (lambda (use-case bound-stx binding-stx)
+                          (printf "RECORD-TOP-LEVEL-ID call.~nuse-case:~n~s~nbound-stx:~n~s~nbinding-stx:~n~s~n"
+                                  use-case
+                                  bound-stx
+                                  binding-stx)))])
+        annotated))
