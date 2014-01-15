4a72a39440d6d027342b72ab947f933dd4927a49
Patrick Boe <pboe@home>
2014-01-14 22:56:49 -0500

    modified scree to inline the litter-with-printfs statement for easier pasting into pasterack for irc sharing

--- scree.rkt
+++ scree.rkt
@@ -1,6 +1,28 @@
 #lang racket
+(begin-for-syntax 
 
-(require (for-syntax "scry.rkt"))
+(require racket/format gui-debugger/annotator) 
+
+(define (litter-with-printfs stx) 
+      (let-values ([(annotated breakmap)
+                    (annotate-stx (expand stx)
+                        (lambda (debug annotated original is-tail?)
+                          (printf "BREAK-WRAP call.~ndebug:~n~s~nannotated:~n~s~noriginal:~n~s~nis-tail?~n~s"
+                                  debug
+                                  annotated
+                                  original
+                                  is-tail?))
+                        (lambda (use-case bound-stx binding-stx)
+                          (printf "RECORD-BOUND-ID call.~nuse-case:~n~s~nbound-stx:~n~s~nbinding-stx:~n~s~n"
+                                  use-case
+                                  bound-stx
+                                  binding-stx))
+                        (lambda (use-case bound-stx binding-stx)
+                          (printf "RECORD-TOP-LEVEL-ID call.~nuse-case:~n~s~nbound-stx:~n~s~nbinding-stx:~n~s~n"
+                                  use-case
+                                  bound-stx
+                                  binding-stx)))])
+        annotated)))
 
 (define-syntax littered
   (lambda (stx)
