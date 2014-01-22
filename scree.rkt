c54ffb7ec092516952bf905ed0cc637c0df5309f
Patrick Boe <pboe@home>
2014-01-22 04:48:35 -0500

    simplified example

--- scree.rkt
+++ scree.rkt
@@ -5,7 +5,4 @@
 (parameterize ([current-namespace (make-base-namespace)])
  (eval-syntax
    (litter-with-printfs
-     (expand-syntax
-       (expand-syntax-to-top-form
-         (namespace-syntax-introduce
-           (datum->syntax #f '(let [(a "bees")] (string-append a "b")))))))))
+     (expand '(let [(a "bees")] (string-append a "b"))))))
