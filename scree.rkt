80169a964fc5b419eceafc1f72ddd72bd3ae1486
Patrick Boe <pboe@home>
2014-01-22 04:40:49 -0500

    can annotate an arbitrary expression

--- scree.rkt
+++ scree.rkt
@@ -1,32 +1,11 @@
-#lang racket
-(begin-for-syntax 
+#lang racket/base
 
-(require racket/format gui-debugger/annotator) 
+(require "scry.rkt")
 
-(define (litter-with-printfs stx) 
-      (let-values ([(annotated breakmap)
-                    (annotate-stx (expand stx)
-                        (lambda (debug annotated original is-tail?)
-                          (printf "BREAK-WRAP call.~ndebug:~n~s~nannotated:~n~s~noriginal:~n~s~nis-tail?~n~s"
-                                  debug
-                                  annotated
-                                  original
-                                  is-tail?))
-                        (lambda (use-case bound-stx binding-stx)
-                          (printf "RECORD-BOUND-ID call.~nuse-case:~n~s~nbound-stx:~n~s~nbinding-stx:~n~s~n"
-                                  use-case
-                                  bound-stx
-                                  binding-stx))
-                        (lambda (use-case bound-stx binding-stx)
-                          (printf "RECORD-TOP-LEVEL-ID call.~nuse-case:~n~s~nbound-stx:~n~s~nbinding-stx:~n~s~n"
-                                  use-case
-                                  bound-stx
-                                  binding-stx)))])
-        annotated)))
-
-(define-syntax littered
-  (lambda (stx)
-    (litter-with-printfs (cadr (syntax->list stx)))))
-
-(define a "hi")
-(littered (string-append a "lo"))
+(parameterize ([current-namespace (make-base-namespace)])
+ (eval-syntax
+   (litter-with-printfs
+     (expand-syntax
+       (expand-syntax-to-top-form
+         (namespace-syntax-introduce
+           (datum->syntax #f '(let [(a "bees")] (string-append a "b")))))))))
--- scry.rkt
+++ scry.rkt
@@ -1,11 +1,12 @@
-#lang racket 
+#lang racket/base
+
 (require racket/format gui-debugger/annotator)
 
 (provide litter-with-printfs)
 
 (define (litter-with-printfs stx) 
       (let-values ([(annotated breakmap)
-                    (annotate-stx (expand stx)
+                    (annotate-stx stx
                         (lambda (debug annotated original is-tail?)
                           (printf "BREAK-WRAP call.~ndebug:~n~s~nannotated:~n~s~noriginal:~n~s~nis-tail?~n~s"
                                   debug
